7cf76c089b0db4da1742aa2f1aa3b516eada7161
Angela <abarreraga@unal.edu.co>
2026-07-27 23:03:08 -0500

    Create Gato.java

---
+++ src/main/java/com/poo/actividadindividual6/punto5/Gato.java
@@ -0,0 +1,24 @@
+package com.poo.actividadindividual6.punto5;
+
+public class Gato extends Felino {
+
+    @Override
+    public String getSonido() {
+        return "Maullido";
+    }
+
+    @Override
+    public String getAlimentos() {
+        return "Ratones";
+    }
+
+    @Override
+    public String getHabitat() {
+        return "Doméstico";
+    }
+
+    @Override
+    public String getNombreCientifico() {
+        return "Felis silvestris catus";
+    }
+}
