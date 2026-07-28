222971cb4c58fa679ab21ef135c1db1a02e8b406
Angela <abarreraga@unal.edu.co>
2026-07-27 22:56:21 -0500

    Create ProfesorTitular.java

---
+++ src/main/java/com/poo/actividadindividual6/Punto3/ProfesorTitular.java
@@ -0,0 +1,9 @@
+package com.poo.actividadindividual6.punto3;
+
+public class ProfesorTitular extends Profesor {
+
+    @Override
+    protected void imprimir() {
+        System.out.println("Es un profesor titular.");
+    }
+}
