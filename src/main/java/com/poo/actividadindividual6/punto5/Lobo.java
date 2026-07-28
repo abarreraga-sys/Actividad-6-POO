d0516d45668aa95ef56405bfbf065a2dc541b4f2
Angela <abarreraga@unal.edu.co>
2026-07-27 23:04:19 -0500

    Create Lobo.java

---
+++ src/main/java/com/poo/actividadindividual6/punto5/Lobo.java
@@ -0,0 +1,24 @@
+package com.poo.actividadindividual6.punto5;
+
+public class Lobo extends Canido {
+
+    @Override
+    public String getSonido() {
+        return "Aullido";
+    }
+
+    @Override
+    public String getAlimentos() {
+        return "Carnívoro";
+    }
+
+    @Override
+    public String getHabitat() {
+        return "Bosque";
+    }
+
+    @Override
+    public String getNombreCientifico() {
+        return "Canis lupus";
+    }
+}
