9c81599fbea9a0863078f2842a85018c15b8a4d6
Angela <abarreraga@unal.edu.co>
2026-07-27 23:00:07 -0500

    Create Prueba3.java

---
+++ src/main/java/com/poo/actividadindividual6/Punto4/Prueba3.java
@@ -0,0 +1,9 @@
+package com.poo.actividadindividual6.punto4;
+
+public class Prueba3 {
+
+    public static void main(String[] args) {
+        Profesor profesor1 = new ProfesorTitular();
+        System.out.println(profesor1);
+    }
+}
