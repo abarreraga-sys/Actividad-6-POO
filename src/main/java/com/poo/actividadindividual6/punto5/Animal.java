1b683cb3cb821fe1b58336500e0bc185ccdca450
Angela <abarreraga@unal.edu.co>
2026-07-27 23:01:06 -0500

    Create Animal.java

---
+++ src/main/java/com/poo/actividadindividual6/punto5/Animal.java
@@ -0,0 +1,17 @@
+package com.poo.actividadindividual6.punto5;
+
+public abstract class Animal {
+
+    protected String sonido;
+    protected String alimentos;
+    protected String habitat;
+    protected String nombreCientifico;
+
+    public abstract String getNombreCientifico();
+
+    public abstract String getSonido();
+
+    public abstract String getAlimentos();
+
+    public abstract String getHabitat();
+}
